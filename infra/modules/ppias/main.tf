015f86c78bc1d9ac61cf7e3fba08b8fd94778a8b
ebouther <ebouther@student.42.fr>
2022-04-07 11:06:02 +0200

    fix: add s3 website configuration

--- infra/modules/ppias/main.tf
+++ infra/modules/ppias/main.tf
@@ -44,6 +44,10 @@ resource "aws_s3_bucket_policy" "this" {
   policy = data.aws_iam_policy_document.this.json
 }
 
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+}
+
 resource "aws_cloudfront_distribution" "this" {
   /*     aliases                        = [
         "paris.pias.science",
