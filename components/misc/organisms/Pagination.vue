096eced2ab488c14ed10d8785a1722bec1f33917
owalid <othmane.el.ayadi@gmail.com>
2022-11-24 13:02:38 +0100

    feat: pagination component, deal with more than 5 pages.

--- components/misc/organisms/Pagination.vue
+++ components/misc/organisms/Pagination.vue
@@ -13,25 +13,45 @@
     >
       <v-icon>mdi-chevron-left</v-icon>
     </v-btn>
+
     <!-- Numbers -->
-    <v-btn
-      v-for="index in length"
-      :key="index"
-      min-width="35"
-      height="35"
-      width="35"
-      text
-      :plain="value !== index"
-      tile
-      outlined
-      :class="{ selected: value === index }"
-      class="mr-2 paginate-btn"
-      :to="index > 1 ? `?page=${index}` : ''"
-      nuxt
-      @click="handleClick(index)"
-    >
-      {{ index }}
-    </v-btn>
+    <template v-if="length <= totalVisible">
+      <v-btn
+        v-for="index in length"
+        :key="index"
+        v-bind="getButtonNumberProps(index)"
+        @click="handleClick(index)"
+      >
+        {{ index }}
+      </v-btn>
+    </template>
+    <template v-else>
+      <v-btn
+        v-for="index in 2"
+        :key="index"
+        v-bind="getButtonNumberProps(index)"
+        @click="handleClick(index)"
+      >
+        {{ index }}
+      </v-btn>
+      <!--  process if current index is between 2 and length - 2 -->
+      <template v-if="value > 2 && value < length - 2">
+        <span class="mr-2">...</span>
+        <v-btn v-bind="getButtonNumberProps(value)">
+          {{ value }}
+        </v-btn>
+        <span class="mr-2">...</span>
+      </template>
+      <template v-else><span class="mr-2">...</span></template>
+      <v-btn
+        v-for="index in [...Array(length).keys()].slice(length - 2, length)"
+        :key="index"
+        v-bind="getButtonNumberProps(index)"
+        @click="handleClick(index)"
+      >
+        {{ index }}
+      </v-btn>
+    </template>
     <!-- Right side -->
     <v-btn
       min-width="35"
@@ -65,8 +85,20 @@ export default {
       required: false,
     },
   },
-
   methods: {
+    getButtonNumberProps(index) {
+      // Return props for button number
+      return {
+        minWidth: '35',
+        height: '35',
+        width: '35',
+        text: true,
+        plain: this.value !== index,
+        tile: true,
+        outlined: true,
+        class: `mr-2 paginate-btn ${this.value === index ? 'selected' : ''}`,
+      }
+    },
     handleClick(page) {
       this.$emit('input', page)
     },
